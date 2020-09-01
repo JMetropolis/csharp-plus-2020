33d49b39786823f45d6498a0e97b10e498b135ad
JCity <jcityes@gmail.com>
2020-09-01 10:01:12 +0200

    update

---
+++ Session03/Session03/Session03Excercise02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Session03Excercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Konvertera det inmatade värdena från strängar till siffror
+            // Resultatet skall presentera:
+            // Lägsta värdet
+            // Högsta värdet
+            // Medelvärde (snitt)
+            // Programmet skall vara felhanterat
+            // Felaktiga värden får inte påverka medelvärde, lägsta eller högsta.
+
+            Console.WriteLine("Ange ett antal siffror, separerat med kommatecken.");
+            var input = Console.ReadLine();
+            var inputArray = input.Split(new [] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var number in inputArray)
+            {
+                Console.WriteLine("Värdet är " + number);
+            }
+
+            int[] numArray = new int[inputArray.Length];
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                numArray[i] = int.Parse(inputArray[i]);
+                Console.WriteLine(numArray[i]);
+            }
+
+            
+
+        }
+    }
+}
